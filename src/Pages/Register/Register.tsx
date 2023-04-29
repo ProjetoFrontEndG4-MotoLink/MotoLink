c80e86495842fe57d304e327b182838a41d7325d
Isabelle <isabelle.bernardes@hotmail.com>
2023-04-29 06:30:07 -0300

    feat: add logic to register conditional forms

--- src/Pages/Register/Register.tsx
+++ src/Pages/Register/Register.tsx
@@ -4,26 +4,51 @@ import { useState } from "react";
 import { RegisterFormMotoboy } from "../../components/RegisterFormMotoboy";
 
 export const Register = () => {
+  const [firstStep, setFirstStep] = useState(true);
   const [formEmpresas, setFormEmpresas] = useState(false);
   const [formMotoboy, setFormMotoboy] = useState(false);
 
+  const backToLogin = () => {
+    setFirstStep(!firstStep);
+    setFormMotoboy(false);
+    setFormEmpresas(false);
+  };
+
+  const openMotoboyForm = () => {
+    setFirstStep(!firstStep);
+    setFormMotoboy(!formMotoboy);
+  };
+
+  const openCompanyForm = () => {
+    setFirstStep(!firstStep);
+    setFormEmpresas(!formEmpresas);
+  };
+
   return (
     <>
-      <h1>Register</h1>
-      <Link to="/"> Voltar para o login</Link>
-
-      {formEmpresas ? (
-        <RegisterFormEmpresas />
-      ) : (
-        <button onClick={() => setFormEmpresas(true)}>Empresa</button>
-      )}
-      
+      {firstStep ? (
+        <div>
+          <h1>Com que objetivo você quer entrar na MotoLink?</h1>
+          <button onClick={() => openMotoboyForm()}>Sou motoboy</button>
+          <button onClick={() => openCompanyForm()}>
+            Sou uma empresa delivery
+          </button>
+        </div>
+      ) : null}
 
       {formMotoboy ? (
-        <RegisterFormMotoboy />
-      ) : (
-        <button onClick={() => setFormMotoboy(true)}>Motoboy</button>
-      )}
+        <>
+          <RegisterFormMotoboy />
+          <Link to="/">Voltar para Login</Link>
+        </>
+      ) : null}
+
+      {formEmpresas ? (
+        <>
+          <RegisterFormEmpresas />
+          <Link to="/">Voltar para Login</Link>
+        </>
+      ) : null}
     </>
   );
 };

fef1ca321b97b22a27671b797c9f8dab3148eb8a
Carlos Senna <carlos_senna@hotmail.com>
2023-05-02 17:33:49 -0300

    fix: error message form AddNewJob

--- src/components/ModalAddNewJobs/index.tsx
+++ src/components/ModalAddNewJobs/index.tsx
@@ -12,11 +12,10 @@ import { NewJobSchema } from "./schema"
 export interface IAddNewJob{
     local:string
     price:number
-    
 }
 
 export const ModalAddNewJob = () => {
-    const {register,handleSubmit}=useForm<IAddNewJob>({})
+    const {register,handleSubmit, formState: {errors}}=useForm<IAddNewJob>({resolver: zodResolver(NewJobSchema)})
     const {addNewJob,setOpenModalAddJob,openModalAddJob}=useContext(JobsContext)
     const submit:SubmitHandler<IAddNewJob>=(formData)=>{
         addNewJob(formData)
@@ -30,10 +29,10 @@ export const ModalAddNewJob = () => {
                     <button onClick={()=>setOpenModalAddJob(false)}>x</button>
                 </div>
                 <form onSubmit={handleSubmit(submit)}>
-                    <Input type="text" id="Endereço" label="Endereço" placeholder="Ex: Rua xxx xxx xx" {...register("local")}/>
-                    <Input  type="number" id="Valor" label="Taxa de Entrega" placeholder="R$00,00" {...register("price")}/>
+                    <Input type="text" id="Endereço" label="Endereço" placeholder="Ex: Rua xxx xxx xx" error={errors.local}{...register("local")}/>
+                    <Input  type="number" id="Valor" label="Taxa de Entrega" placeholder="R$00,00" error={errors.price}{...register("price")}/>
                     <ButtonDefault 
-                    type="button"
+                    type="submit"
                     className="fullWidth"
                     buttonSize="default"
                     buttonStyle="yellow"

86bcaad517d14ee23a9e6f0cec787e93a408ce48
Carlos Senna <carlos_senna@hotmail.com>
2023-05-02 15:25:04 -0300

    feat: header 1/2

--- src/components/Header/DashboardHeaderStyle.ts
+++ src/components/Header/DashboardHeaderStyle.ts
@@ -13,7 +13,37 @@ export const DashboardHeaderStyle = styled.header`
   box-shadow: 0px 2px 8px 2px rgba(40, 51, 0, 0.1);
 
   .buttonsContainer {
+    display: none;
+  }
+
+  .button__menu {
+    img {
+      width: 20px;
+      height: auto;
+    }
+  }
+
+  .container {
+    width: 100%;
     display: flex;
-    gap: 10px;
+    justify-content: space-between;
+  }
+
+  
+  @media (min-width: 1024px) {
+    .container {
+      width: 1180px;
+      max-width: 100%;
+    }
+
+    .buttonsContainer {
+      display: flex;
+      gap: 10px;
+    }
+
+    .button__menu {
+      display: none;
+    }
+
   }
 `;
